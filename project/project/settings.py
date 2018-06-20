b6f37ac13b6225af2a46a76a942df2506bcf1cfc
Donapaulson <40259664+Donapaulson@users.noreply.github.com>
2018-06-20 16:58:19 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,6 +14,9 @@ BOT_NAME = 'project'
 SPIDER_MODULES = ['project.spiders']
 NEWSPIDER_MODULE = 'project.spiders'
 
+#Export as CSV Feed
+FEED_FORMAT = "csv"
+FEED_URI = "blablacar.csv"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'project (+http://www.yourdomain.com)'
